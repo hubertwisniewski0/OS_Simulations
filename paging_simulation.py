e2d8c3663dd878e3042f3159fb216d7ba27b3a5a
Hubert Wiśniewski <263815@student.pwr.edu.pl>
2023-01-07 15:24:13 +0100

    Add paging simulation program and test data

---
+++ paging_simulation.py
@@ -0,0 +1,14 @@
+import json
+from argparse import ArgumentParser
+from paging.MemoryManagerSimulation import MemoryManagerSimulation
+from utils.Encoder import Encoder
+
+ap = ArgumentParser()
+ap.add_argument('input_file', help='Data input file')
+args = ap.parse_args()
+
+simulation = MemoryManagerSimulation()
+simulation.read_data(args.input_file)
+simulation.simulate()
+
+print(json.dumps(simulation, indent=4, cls=Encoder))
