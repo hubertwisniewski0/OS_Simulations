58a2a61f60dbf483e541098a1018baf016d06e77
Hubert Wiśniewski <263815@student.pwr.edu.pl>
2023-01-20 12:49:10 +0100

    Add a simple scheduler input data generator

---
+++ scheduler_generator.py
@@ -0,0 +1,17 @@
+import json
+from random import Random
+from typing import List
+from sched.sim.SimulationDescription import SimulationDescription
+from utils.Encoder import Encoder
+
+simulations: List[SimulationDescription] = []
+rng = Random()
+
+for i in range(100):
+    desc = SimulationDescription()
+    desc.task_list = [{"come_time": rng.randint(0, 99), "duration": rng.randint(1, 20)} for j in range(100)]
+    desc.lottery_seed = rng.getrandbits(64)
+    simulations.append(desc)
+
+with open('gen_out.json', 'wt') as f:
+    json.dump(simulations, f, indent=4, cls=Encoder)
