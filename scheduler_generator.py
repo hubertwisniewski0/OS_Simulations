1ef20421aa0046c426d9d50e2d7f4cc776f29ab0
Hubert Wiśniewski <263815@student.pwr.edu.pl>
2023-01-20 13:04:27 +0100

    Use TaskBase in scheduler_generator

--- scheduler_generator.py
+++ scheduler_generator.py
@@ -1,6 +1,7 @@
 import json
 from random import Random
 from typing import List
+from sched.core.TaskBase import TaskBase
 from sched.sim.SimulationDescription import SimulationDescription
 from utils.Encoder import Encoder
 
@@ -9,7 +10,7 @@ rng = Random()
 
 for i in range(100):
     desc = SimulationDescription()
-    desc.task_list = [{"come_time": rng.randint(0, 99), "duration": rng.randint(1, 20)} for j in range(100)]
+    desc.task_list = [TaskBase(rng.randint(0, 99), rng.randint(1, 20)) for j in range(100)]
     desc.lottery_seed = rng.getrandbits(64)
     simulations.append(desc)
 
