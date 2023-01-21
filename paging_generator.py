b5650f25fa79312fe8575bb60efb1d1361c20270
Hubert Wiśniewski <263815@student.pwr.edu.pl>
2023-01-21 21:34:41 +0100

    Add argument support to paging_generator

--- paging_generator.py
+++ paging_generator.py
@@ -1,17 +1,38 @@
 import json
-from random import Random
+from argparse import ArgumentParser
 from typing import List
 from paging.sim.SimulationDescription import SimulationDescription
 from utils.Encoder import Encoder
+from utils.ExtendedRandom import ExtendedRandom
+
+ap = ArgumentParser(description='Generate paging simulation input data',
+                    epilog='NOTE: values generated using Gaussian distribution are still confined to their respective '
+                           'bounds!')
+ap.add_argument('output_file', type=str, help='Generated data output file')
+ap.add_argument('-n', '--simulations', type=int, default=100, help='Number of separate simulations (default: 100)')
+ap.add_argument('-l', '--simulation-length', type=int, default=100,
+                help='Number of page accesses in a single simulation (default: 100)')
+ap.add_argument('-p', '--page-access', nargs=2, type=int, metavar=('MIN', 'MAX'), default=[0, 19],
+                help='Accessed page number bounds (inclusive, default: 0 19)')
+ap.add_argument('-g', '--gaussian-page-access', nargs=2, type=float, metavar=('MEAN', 'STDDEV'),
+                help='Use normal (Gaussian) distribution to generate accessed page number (default: use uniform '
+                     'distribution)')
+ap.add_argument('-m', '--memory-sizes', nargs='+', type=int, metavar='SIZE', default=[3, 5, 7],
+                help='Memory sizes to use for simulation (default: 3 5 7)')
+ap.add_argument('-s', '--seed', help='Seed for the random number generator (default: use the current system time)')
+args = ap.parse_args()
+
+rng = ExtendedRandom(args.seed)
 
 simulations: List[SimulationDescription] = []
-rng = Random()
 
-for i in range(100):
+for i in range(args.simulations):
     desc = SimulationDescription()
-    desc.access_list = [rng.randint(0, 19) for j in range(100)]
-    desc.memory_sizes = [3, 5, 7]
+    desc.access_list = [rng.bounded_int_gaussian(args.page_access[0], args.page_access[1], args.gaussian_page_access[0],
+                                                 args.gaussian_page_access[1]) if args.gaussian_page_access else
+                        rng.randint(args.page_access[0], args.page_access[1]) for j in range(args.simulation_length)]
+    desc.memory_sizes = args.memory_sizes
     simulations.append(desc)
 
-with open('gen_out.json', 'wt') as f:
+with open(args.output_file, 'wt') as f:
     json.dump(simulations, f, indent=4, cls=Encoder)
